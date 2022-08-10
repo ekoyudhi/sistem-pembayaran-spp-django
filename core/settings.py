bc471eab67020c9a97a46990c114d2a009a80325
ekoyudhi <ekoyudhiprastowo@gmail.com>
2022-08-10 14:10:04 +0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'), 'aplikasispprsurya.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'), '.herokuapp.com']
 
 # Application definition
 
